97e1fcb151ede452becb41f90e9ad0dd76202765
samidevrimoyar <sami.devrim@gmail.com>
2025-06-27 14:44:12 +0300

    update

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -3,7 +3,7 @@ URL mappings for the recipe app.
 """
 from django.urls import (
     path,
-    include
+    include,
 )
 
 from rest_framework.routers import DefaultRouter
@@ -12,7 +12,7 @@ from recipe import views
 
 
 router = DefaultRouter()
-router.register('recipes', views.ReciveViewSet)
+router.register('recipes', views.RecipeViewSet)
 
 app_name = 'recipe'
 
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -9,15 +9,15 @@ from core.models import Recipe
 from recipe import serializers
 
 
-class ReciveViewSet(viewsets.ModelViewSet):
+class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
-    serializer_class = serializers.RecipeSerializer
+    serializer_class = serializers.RecipeDetailSerializer
     queryset = Recipe.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        """Retrieve recipes for authenticated users."""
+        """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
 
     def get_serializer_class(self):
@@ -26,3 +26,7 @@ class ReciveViewSet(viewsets.ModelViewSet):
             return serializers.RecipeSerializer
 
         return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save(user=self.request.user)
